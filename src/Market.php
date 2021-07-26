7f9ff3faf087e6253887586c30f9ee4097563c31
柳周侯爷 <sjywz@foxmail.com>
2021-07-26 17:33:55 +0800

    优化初始化

--- src/Market.php
+++ src/Market.php
@@ -2,15 +2,45 @@
 
 namespace Lzhy\Cloudmarket;
 
-use Lzhy\Cloudmarket\Support\Cloud;
+use Lzhy\Cloudmarket\Support\Bce;
+use Lzhy\Cloudmarket\Support\Hce;
+use Lzhy\Cloudmarket\Support\Jd;
+use Lzhy\Cloudmarket\Support\Ksyun;
+use Lzhy\Cloudmarket\Support\Tce;
 
 class Market
 {
     protected $cloudmarket;
+    protected $supportMarket = ['bce','ksyun','hce','jd','tce'];
 
-    public function __construct(Cloud $cloudmarket)
+    function __construct($market,$token)
     {
-        $this->cloudmarket = $cloudmarket;
+        if(empty($market) || empty($token)){
+            throw new \Exception('market or token is required');
+        }
+        $market = strtolower($market);
+        if(!in_array($market,$this->supportMarket)){
+            throw new \Exception('current market is not support');
+        }
+
+        switch($market){
+            case 'bce':
+                $this->cloudmarket = new Bce($token);
+                break;
+            case 'ksyun':
+                $this->cloudmarket = new Ksyun($token);
+                break;
+            case 'hce':
+                $this->cloudmarket = new Hce($token);
+                break;
+            case 'jd':
+                $this->cloudmarket = new Jd($token);
+                break;
+            case 'tce':
+                $this->cloudmarket = new Tce($token);
+            default:
+                throw new \Exception('current market is not support');
+        }
     }
 
     public function input($parse = true)
